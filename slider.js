8f5c124857e43f6a099c90732fcd77511dcfafdc
vasilije.knezevic <vasilije.upwork@gmail.com>
2021-05-14 11:16:02 +0200

    additional styles

--- slider.js
+++ slider.js
@@ -11,5 +11,5 @@
           slideIndex++;
           if (slideIndex > slides.length) {slideIndex = 1}
           slides[slideIndex-1].style.display = "block";
-          setTimeout(showSlides,4000); 
+          setTimeout(showSlides,2000); 
         }
